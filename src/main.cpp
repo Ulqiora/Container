85df786348e4582f01c48b8a56d97c14eefa46ba
jcraster <jcraster@student.21-school.ru>
2022-03-05 19:12:30 +0700

    Map start

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,6 @@
 #include <gtest/gtest.h>
 
 int main(){
-    std::list<char> a{1,3,5,7,9,11};
-    s21::list<int> b{2,4,6,8,10,12,14};
-    std::cout << a.max_size() << std::endl;
-    unsigned long long max=std::numeric_limits<int>::max();
-    printf("%Ld",max);
-    return 0;
+    std::pair<int,float> f{1,1.3};
+    
 }
