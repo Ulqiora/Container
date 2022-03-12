a3fe64024de0faa01ca474e600efce3d6d05f031
phawkgir <phawkgir@student.21-school.ru>
2022-03-12 13:25:02 +0700

    explicit pair fix

--- src/pair.h
+++ src/pair.h
@@ -9,7 +9,7 @@ struct pair : public std::pair<T1, T2> {
     pair();
     pair(T1 t1, T2 t2);
     pair(const pair<T1, T2>& other);
-    explicit pair(const std::pair<T1, T2>& other);
+    pair(const std::pair<T1, T2>& other);
     pair<T1, T2>& operator=(const pair<T1, T2>& other);
     pair<T1, T2>& operator=(pair<T1, T2>&& other);
     bool operator<(const pair<T1, T2>& other) const;
